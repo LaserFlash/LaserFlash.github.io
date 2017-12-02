bacae40a9a3108e597f985de8019d8473c8ab207
Bryn Bennett <bryn.t.bennett@gmail.com>
2017-12-02 16:56:37 +1300

    production ready portfolio view

---
+++ src/app/portfolio-view/portfolio-view.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { PortfolioItem } from '../portfolioItem'
+import { PortfolioButton } from '../portfolioButton'
+
+import { PortfolioService } from '../portfolio.service'
+
+@Component({
+  selector: 'app-portfolio-view',
+  templateUrl: './portfolio-view.component.html',
+  styleUrls: ['./portfolio-view.component.css']
+})
+export class PortfolioViewComponent implements OnInit {
+  items: PortfolioItem[];
+  constructor(portfolioService:PortfolioService) {
+    this.items = portfolioService.portfolioItems;
+  }
+
+  ngOnInit() {
+  }
+
+}
