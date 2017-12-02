11b96f2210eb2080b76596b2b483beccca954330
Bryn Bennett <bryn.t.bennett@gmail.com>
2017-12-02 20:42:33 +1300

    deploy script with custom domain

--- deployWebsite.bat
+++ deployWebsite.bat
@@ -1,5 +1,3 @@
 call ./buildWebsite
-call git reset
-call git add dist -f
-call git commit -m "Generated page update"
-call git subtree push --prefix dist origin master
+call echo laserflash.tk > dist/CNAME
+call ngh --branch=master
