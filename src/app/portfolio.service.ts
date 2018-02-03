d4314696436c0c37ff2ece0cd3f79680ff4c94ec
Bryn Bennett <bryn.t.bennett@gmail.com>
2018-02-03 17:46:47 +1300

    fix: images not loading webp was only supported by chrome and opera so have now moved back to using png images for increased compatability

--- src/app/portfolio.service.ts
+++ src/app/portfolio.service.ts
@@ -9,7 +9,7 @@ export class PortfolioService {
     this.portfolioItems.push(new PortfolioItem(
       "U23 Squad Breakages",
       "Data base type thing using Angular5 and FireBase, will be used to keep track of maintenance for the Wellington Youth Sailing Trust boats.",
-      "assets/images/nespresso.webp",
+      "assets/images/nespresso.png",
       new PortfolioButton("Source","https://github.com/LaserFlash/WYST-Maintenance"),
       new PortfolioButton("View","https://wyst-stuff.tk")
     ))
@@ -17,7 +17,7 @@ export class PortfolioService {
     this.portfolioItems.push(new PortfolioItem(
       "DimThing",
       "Program that allows for further dimming of screen through the use of a transparent overlay. Has support for keyboard shortcuts to adjust dimness and toggle immersive mode on or off.",
-      "assets/images/DimThing.webp",
+      "assets/images/DimThing.png",
       new PortfolioButton("Source","https://github.com/LaserFlash/DimThing"),
       null
     ))
@@ -25,7 +25,7 @@ export class PortfolioService {
     this.portfolioItems.push(new PortfolioItem(
       "Portfolio Site",
       "Construction and design of this website. This project tends to get bursts of updates as it is just worked on by me. It is regularly revisted however",
-      "assets/images/flatWebsite.webp",
+      "assets/images/flatWebsite.png",
       new PortfolioButton("Source","https://github.com/LaserFlash/LaserFlash.github.io"),
       new PortfolioButton("View","https://laserflash.tk")
     ))
@@ -33,7 +33,7 @@ export class PortfolioService {
     this.portfolioItems.push(new PortfolioItem(
       "ENGR 110 SCARA",
       "This is a group project, to implement basic drawing functionality to a provided Raspberry Pi based SCARA using java code.",
-      "assets/images/SCARA.webp",
+      "assets/images/SCARA.png",
       new PortfolioButton("Source","https://github.com/LaserFlash/ENGR110-SCARA"),
       null
     ))
@@ -41,7 +41,7 @@ export class PortfolioService {
     this.portfolioItems.push(new PortfolioItem(
       "ENGR 101 Autonomous Vehicle",
       "This was a group project, with the goal of designing and contructing a small autonomous vehicle to get through given sections of a maze.",
-      "assets/images/AVC.webp",
+      "assets/images/AVC.png",
       new PortfolioButton("Source","https://github.com/papasele/AVCteam1"),
       null
     ))
