3ecd0874846afc08de386f073ab16cec84f718a2
Bryn Bennett <bryn.t.bennett@gmail.com>
2019-07-08 16:25:03 +1200

    Lint fixes

--- webp.js
+++ webp.js
@@ -1,4 +1,4 @@
-var outputFolder = "./src/assets/images/webp" // Output folder    
+const outputFolder = './src/assets/images/webp'; // Output folder
 
 const imagemin = require('imagemin');
 const imageminWebp = require('imagemin-webp');
@@ -6,9 +6,9 @@ const imageminWebp = require('imagemin-webp');
 imagemin(['./src/assets/images/*.{jpg,png}'], outputFolder, {
   use: [
     imageminWebp({
-      quality: 50
-    })
-  ]
+      quality: 50,
+    }),
+  ],
 }).then(() => {
   console.log('Images optimized');
 });
