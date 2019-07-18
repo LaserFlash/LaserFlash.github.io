2233c0663278a5a2904cdba0ea0b9f10423d0ead
Bryn Bennett <bryn.t.bennett@gmail.com>
2019-07-18 16:26:28 +1200

    Code Styling: remove non required imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,61 +10,70 @@ import { routerTransition } from './router.animations';
 import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
-  animations: [ routerTransition ],
+  animations: [routerTransition],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   router: Router;
 
+  inkColour = '#087f23';
+
   title = 'Bryn Bennett "LaserFlash"';
 
   routeLinks = [
     { label: 'Portfolio', link: '/portfolio' },
-    { label: 'About', link: '/about' },
+    { label: 'About', link: '/about' }
   ];
 
   SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
 
-  constructor(private _router: Router ) {
+  constructor(private _router: Router) {
     this.router = _router;
   }
 
   ngOnInit() {
-    this.router.events.subscribe((evt) => {
+    this.router.events.subscribe(evt => {
       if (!(evt instanceof NavigationEnd)) {
-          return;
+        return;
       }
       const element = document.getElementById('scrollId');
       setTimeout(function() {
         element.scrollIntoView();
         window.scrollTo(0, 0);
-    }, 300);
-
-  });
+      }, 300);
+    });
   }
 
-
-
   swipe(action = this.SWIPE_ACTION.RIGHT) {
-      const pos: any = this.routeLinks.filter((item) => {if (item.link === this.router.url) {return item; } });
-      const currentIndex: number = this.routeLinks.indexOf(pos[0]);
+    const pos: any = this.routeLinks.filter(item => {
+      if (item.link === this.router.url) {
+        return item;
+      }
+    });
+    const currentIndex: number = this.routeLinks.indexOf(pos[0]);
 
-      if (currentIndex > this.routeLinks.length || currentIndex < 0) { return; }
-      let nextIndex = 0;
-      // next
-      if (action === this.SWIPE_ACTION.RIGHT) {
-        nextIndex = currentIndex - 1;
-      }
-
-      // previous
-      if (action === this.SWIPE_ACTION.LEFT) {
-        nextIndex = currentIndex + 1;
-      }
-      if (nextIndex >= 0 && nextIndex < this.routeLinks.length && nextIndex !== currentIndex) {
-        this.router.navigateByUrl(this.routeLinks[nextIndex].link);
-      }
+    if (currentIndex > this.routeLinks.length || currentIndex < 0) {
+      return;
     }
+    let nextIndex = 0;
+    // next
+    if (action === this.SWIPE_ACTION.RIGHT) {
+      nextIndex = currentIndex - 1;
+    }
+
+    // previous
+    if (action === this.SWIPE_ACTION.LEFT) {
+      nextIndex = currentIndex + 1;
+    }
+    if (
+      nextIndex >= 0 &&
+      nextIndex < this.routeLinks.length &&
+      nextIndex !== currentIndex
+    ) {
+      this.router.navigateByUrl(this.routeLinks[nextIndex].link);
+    }
+  }
 
   getState(outlet) {
     return outlet.activatedRouteData.state;
