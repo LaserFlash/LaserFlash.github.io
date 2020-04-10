b0ca73eaaf34b5edf6b1f43283745348af26fdbd
LaserFlash <bryn.t.bennett@gmail.com>
2020-04-10 18:53:41 +1200

    Lint fixes

--- src/app/hammer.config.ts
+++ src/app/hammer.config.ts
@@ -1,7 +1,7 @@
 import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 
 import * as Hammer from 'hammerjs';
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class HammerConfig extends HammerGestureConfig {
