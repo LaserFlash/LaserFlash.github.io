773302d61ece304df378d64550e5ae8da83b3a6a
Bryn Bennett <bryn.t.bennett@gmail.com>
2021-01-13 20:48:02 +1300

    CHORE: update angular/core

--- src/app/about-view/about-view.component.spec.ts
+++ src/app/about-view/about-view.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AboutViewComponent } from './about-view.component';
 
@@ -6,7 +6,7 @@ describe('AboutViewComponent', () => {
   let component: AboutViewComponent;
   let fixture: ComponentFixture<AboutViewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [AboutViewComponent]
     }).compileComponents();
--- src/app/language-icon/language-icon.component.spec.ts
+++ src/app/language-icon/language-icon.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LanguageIconComponent } from './language-icon.component';
 
@@ -6,7 +6,7 @@ describe('LanguageIconComponent', () => {
   let component: LanguageIconComponent;
   let fixture: ComponentFixture<LanguageIconComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [LanguageIconComponent]
     }).compileComponents();
